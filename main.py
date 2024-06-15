1ca3d0b0c8be101d3a9423e0a764df317c681ca2
Raynald Krisnawan <raynaldkris@gmail.com>
2024-06-15 12:41:16 +0700

    Update the model and change logic
    Update to latest model, add logic to use ,h5 model, add logic to preprocess image so the model can use it

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import traceback
 import tensorflow as tf
 import numpy as np
 import requests
+import string
+from PIL import Image
 from pydantic import BaseModel
 from fastapi import FastAPI, Response, UploadFile
 from utils import load_image_into_numpy_array
+from google_storage import upload_image
+from dotenv import load_dotenv
+load_dotenv()
 
-public_model_url = "https://storage.googleapis.com/tes_model/Model_1.zip"
+model_url = os.environ.get('MODEL_URL')
 local_model_path = "./"
 
 def download_and_unzip_model(url, dest_path):
@@ -28,40 +33,33 @@ def download_and_unzip_model(url, dest_path):
     
     os.remove(zip_path)
     print(f"Model downloaded and extracted to {dest_path}")
+    
+def download_modelh5(url, dest_path):
+    if not os.path.exists(dest_path):
+        os.makedirs(dest_path)
+    
+    model_path = os.path.join(dest_path, "model.h5")
+    
+    response = requests.get(url, stream=True)
+    with open(model_path, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=128):
+            f.write(chunk)
+    
+    print(f"Model downloaded to {dest_path}")
 
-download_and_unzip_model(public_model_url, local_model_path)
+# Uncomment if the model is in cloud
+# download_and_unzip_model(model_url, local_model_path) # saved_model format
+download_modelh5(model_url, local_model_path) # h5 format
 
-model = tf.saved_model.load("./saved_model")
-model_signature = model.signatures["serving_default"]
+# if use saved model
+# model = tf.saved_model.load("./saved_model")
+# model_signature = model.signatures["serving_default"]
 
-class_name = {
-    0: "A",
-    1: "B",
-    2: "C",
-    3: "D",
-    4: "E",
-    5: "F",
-    6: "G",
-    7: "H",
-    8: "I",
-    9: "J",
-    10: "K",
-    11: "L",
-    12: "M",
-    13: "N",
-    14: "O",
-    15: "P",
-    16: "Q",
-    17: "R",
-    18: "S",
-    19: "T",
-    20: "U",
-    21: "V",
-    22: "W",
-    23: "X",
-    24: "Y",
-    25: "Z",
-}
+# If use h5 type uncomment line below
+model = tf.keras.models.load_model('./model.h5')
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+class_name = list(string.ascii_uppercase)
 
 app = FastAPI()
 
@@ -74,34 +72,45 @@ def predict_image(imageFile: UploadFile, response: Response):
     try:
         if imageFile.content_type not in ["image/jpeg", "image/png"]:
             response.status_code = 400
-            return "File is Not an Image"
+            return "File is not an image"
         
         image = load_image_into_numpy_array(imageFile.file.read())
-        print("Image shape:", image.shape)
         
-
         img_height, img_width = 224, 224 
-        image = tf.image.resize(image, (img_height, img_width))
+        image = image.resize((img_height, img_width))
+        image = np.array(image)
         image = np.expand_dims(image, axis=0)
-        image = image / 255.0 
+        image = tf.keras.applications.vgg16.preprocess_input(image)
         
-        input_tensor = tf.convert_to_tensor(image)
-        result = model_signature(input_tensor)
-        
-        predictions = result['dense_1'] 
+        predictions = model.predict(image)
         print(predictions)
-        print(max(predictions[0]))
         
-        predicted_class = np.argmax(predictions, axis=1)
-        predicted_class_name = class_name.get(predicted_class[0])
+        predicted_class = np.argmax(predictions, axis=1)[0]
+        predicted_class_name = class_name[predicted_class]
+        confidence_score = np.max(predictions, axis=1)[0]
         
-        return {"predicted_class": int(predicted_class[0]), "class_name": predicted_class_name}
+        # Store Image to Cloud Storage uncomment if in prod
+        # local_image_path = f"./uploads/{predicted_class_name}/{imageFile.filename}"
+        # os.makedirs(os.path.dirname(local_image_path), exist_ok=True)
+        # with open(local_image_path, "wb") as f:
+        #     f.write(imageFile.file.read())
+            
+        # bucket_name = os.environ.get('BUCKET_NAME')
+        # destination_blob_name = f"uploads/{predicted_class_name}/{imageFile.filename}"
+        # upload_image(bucket_name, local_image_path, destination_blob_name)
+        # os.remove(local_image_path)
+        
+        return {
+            "predicted_class": int(predicted_class), 
+            "class_name": predicted_class_name,
+            "confidence_score": float(confidence_score)
+        }
     except Exception as e:
         traceback.print_exc()
         response.status_code = 500
         return "Internal Server Error"
 
-
-port = os.environ.get("PORT", 8080)
-print(f"Listening to http://0.0.0.0:{port}")
-uvicorn.run(app, host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    print(f"Listening to http://0.0.0.0:{port}")
+    uvicorn.run(app, host='0.0.0.0', port=port)
--- utils.py
+++ utils.py
@@ -4,4 +4,5 @@ from PIL import Image
 from io import BytesIO
 
 def load_image_into_numpy_array(data):
-    return np.array(Image.open(BytesIO(data)))
+    image = Image.open(BytesIO(data))
+    return image
