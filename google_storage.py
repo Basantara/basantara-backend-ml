cf02e3dfd8b959e8a0daf33acae0873256827d43
Raynald Krisnawan <raynaldkris@gmail.com>
2024-06-16 01:57:03 +0700

    debug
    debuging when user uploading same name file as the file in GCS it will not overwrite the old data

--- google_storage.py
+++ google_storage.py
@@ -6,6 +6,15 @@ def upload_image(bucket_name, source_file_name, destination_blob_name):
 
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
+    
+    if blob.exists():
+        base, extension = os.path.splitext(destination_blob_name)
+        counter = 1
+        while blob.exists():
+            new_blob_name = f"{base}_{counter}{extension}"
+            blob = bucket.blob(new_blob_name)
+            counter += 1
+        destination_blob_name = new_blob_name
 
     blob.upload_from_filename(source_file_name)
 
