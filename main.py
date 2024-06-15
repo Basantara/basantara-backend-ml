295fcb1d211db07caf63fe44bf92f0be20a0bbac
Raynald Krisnawan <raynaldkris@gmail.com>
2024-06-16 00:36:28 +0700

    Add google storage, change treshold, change response, change docker, Add CI/CD
    Add google storage to save uploaded image, change treshold from 0.5 to 0.2 (ML team request), change response structure, change dockerfile to fetch model before cloud run running to optimize latency issue, add CI/CD for dev branch

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import string
 from PIL import Image
 from pydantic import BaseModel
 from fastapi import FastAPI, Response, UploadFile
-from utils import load_image_into_numpy_array
+from utils import load_image
 from google_storage import upload_image
 from dotenv import load_dotenv
 load_dotenv()
@@ -16,45 +16,41 @@ load_dotenv()
 model_url = os.environ.get('MODEL_URL')
 local_model_path = "./"
 
-def download_and_unzip_model(url, dest_path):
-    if not os.path.exists(dest_path):
-        os.makedirs(dest_path)
+# PS: To reduce latency, download the model when building image, this code not deleted in case the model is saved_model
+# def download_and_unzip_model(url, dest_path):
+#     if not os.path.exists(dest_path):
+#         os.makedirs(dest_path)
     
-    zip_path = os.path.join(dest_path, "model.zip")
+#     zip_path = os.path.join(dest_path, "model.zip")
     
-    response = requests.get(url, stream=True)
-    with open(zip_path, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=128):
-            f.write(chunk)
+#     response = requests.get(url, stream=True)
+#     with open(zip_path, 'wb') as f:
+#         for chunk in response.iter_content(chunk_size=128):
+#             f.write(chunk)
     
-    import zipfile
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(dest_path)
+#     import zipfile
+#     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+#         zip_ref.extractall(dest_path)
     
-    os.remove(zip_path)
-    print(f"Model downloaded and extracted to {dest_path}")
+#     os.remove(zip_path)
+#     print(f"Model downloaded and extracted to {dest_path}")
     
-def download_modelh5(url, dest_path):
-    if not os.path.exists(dest_path):
-        os.makedirs(dest_path)
+# def download_modelh5(url, dest_path):
+#     if not os.path.exists(dest_path):
+#         os.makedirs(dest_path)
     
-    model_path = os.path.join(dest_path, "model.h5")
+#     model_path = os.path.join(dest_path, "model.h5")
     
-    response = requests.get(url, stream=True)
-    with open(model_path, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=128):
-            f.write(chunk)
+#     response = requests.get(url, stream=True)
+#     with open(model_path, 'wb') as f:
+#         for chunk in response.iter_content(chunk_size=128):
+#             f.write(chunk)
     
-    print(f"Model downloaded to {dest_path}")
-    
-# Threshold Logic
-def is_valid_prediction(prediction, threshold=0.5):
-    confidence = np.max(prediction)
-    return confidence > threshold
+#     print(f"Model downloaded to {dest_path}")    
 
 # Uncomment if the model is in cloud
 # download_and_unzip_model(model_url, local_model_path) # saved_model format
-download_modelh5(model_url, local_model_path) # h5 format
+# download_modelh5(model_url, local_model_path) # h5 format
 
 # if use saved model
 # model = tf.saved_model.load("./saved_model")
@@ -65,6 +61,7 @@ model = tf.keras.models.load_model('./model.h5')
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 class_name = list(string.ascii_uppercase)
+bucket_name = os.environ.get('BUCKET_NAME')
 
 app = FastAPI()
 
@@ -79,7 +76,7 @@ def predict_image(imageFile: UploadFile, response: Response):
             response.status_code = 400
             return "File is not an image"
         
-        image = load_image_into_numpy_array(imageFile.file.read())
+        image = load_image(imageFile.file.read())
         
         img_height, img_width = 224, 224 
         image = image.resize((img_height, img_width))
@@ -95,30 +92,50 @@ def predict_image(imageFile: UploadFile, response: Response):
         confidence_score = np.max(predictions, axis=1)[0]
         
         # Store Image to Cloud Storage uncomment if in prod
-        # local_image_path = f"./uploads/{predicted_class_name}/{imageFile.filename}"
-        # os.makedirs(os.path.dirname(local_image_path), exist_ok=True)
-        # with open(local_image_path, "wb") as f:
-        #     f.write(imageFile.file.read())
-            
-        # bucket_name = os.environ.get('BUCKET_NAME')
-        # destination_blob_name = f"uploads/{predicted_class_name}/{imageFile.filename}"
-        # upload_image(bucket_name, local_image_path, destination_blob_name)
-        # os.remove(local_image_path)
+        
         if is_valid_prediction(confidence_score):
+            uploadPredictionImage(True, predicted_class_name=predicted_class_name, imageFile=image)
             return {
-                "predicted_class": int(predicted_class), 
-                "class_name": predicted_class_name,
-                "confidence_score": float(confidence_score)
+                "status": "Success",
+                "message": "Model succesfully predict the image",
+                "data": {
+                    "predicted_class": int(predicted_class), 
+                    "class_name": predicted_class_name,
+                    "confidence_score": float(confidence_score)
+                    },
             }
         else:
+            uploadPredictionImage(False, predicted_class_name=predicted_class_name, imageFile=image)
             return {
+                "status": "Fail",
                 "message": "Confidence Score Under Threshold",
-                "confidence_score": float(confidence_score)
+                "data": {
+                    "confidence_score": float(confidence_score)
+                    },
+                
             }
     except Exception as e:
         traceback.print_exc()
         response.status_code = 500
         return "Internal Server Error"
+    
+# Threshold Logic
+def is_valid_prediction(prediction, threshold=0.2):
+    confidence = np.max(prediction)
+    return confidence > threshold
+
+def uploadPredictionImage(validPred: bool, predicted_class_name: string, imageFile: Image ):
+    if(not validPred):
+        predicted_class_name = "unknown"
+    
+    local_image_path = f"./uploads/{predicted_class_name}/{imageFile.filename}"
+    os.makedirs(os.path.dirname(local_image_path), exist_ok=True)
+    with open(local_image_path, "wb") as f:
+        f.write(imageFile.file.read())
+            
+    destination_blob_name = f"uploads/{predicted_class_name}/{imageFile.filename}"
+    upload_image(bucket_name, local_image_path, destination_blob_name)
+    os.remove(local_image_path)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
--- utils.py
+++ utils.py
@@ -3,6 +3,6 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 
-def load_image_into_numpy_array(data):
+def load_image(data):
     image = Image.open(BytesIO(data))
     return image
